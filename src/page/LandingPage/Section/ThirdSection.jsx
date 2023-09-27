f4c682c98580982c6b51537c4e19cd2c91220e78
Mr-Lucasz <l.cunha14.lc@gmail.com>
2023-09-27 17:55:20 -0300

    Contatos Section

--- src/page/LandingPage/Section/ThirdSection.jsx
+++ src/page/LandingPage/Section/ThirdSection.jsx
@@ -1,8 +1,60 @@
-// import styles from './ThirdSection.module.css';
+import styles from "./ThirdSection.module.css";
 import { TitleTopic } from "../../../components/util/TitleTopic";
+import { Heading3 } from "../../../components/util/Heading3";
+import TextField from "@mui/material/TextField";
+import { Button } from "../../../components/util/Button";
+import { styled } from "@mui/material/styles";
 
 export default function ThirdSection() {
-    return (
-   <TitleTopic title="Contato" id="contacts" />
-    );
-    }
+  const CssTextField = styled(TextField)({
+    "& label.Mui-focused": {
+      color: "white",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "white",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "White",
+      },
+      "&:hover fieldset": {
+        borderColor: "white",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "white",
+      },
+      "& label.Mui-focused": {
+        color: "white",
+    },
+    },
+  });
+  return (
+    <>
+      <TitleTopic title="Contato" id="contacts" />
+      <form className={styles.formContatos}>
+        <Heading3 color="white" fontSize="large" text="Informações pessoais:" />
+        <CssTextField
+          id="outlined-basic"
+          label="Informe seu Nome"
+          variant="outlined"
+          fullWidth
+          
+        />
+        <CssTextField
+          id="outlined-basic"
+          label="Informe seu Número"
+          variant="outlined"
+          fullWidth
+        />
+        <Heading3 color="white" fontSize="large" text="Assunto de Interesse" />
+        <CssTextField
+          id="outlined-basic"
+          label="Informe sua mensagem"
+          variant="outlined"
+          fullWidth
+        />
+        <Button color="red" buttonName="ENVIAR" />
+      </form>
+    </>
+  );
+}
