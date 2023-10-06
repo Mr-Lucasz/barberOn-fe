20898b0c5f008f5703b9c2a65cbbcc6c40e07fd3
Mr-Lucasz <l.cunha14.lc@gmail.com>
2023-10-05 22:49:02 -0300

    Alterações Landing page

---
+++ src/page/Home/HomePage.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import events from "./events";
+import styles from "./HomePage.module.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+moment.locale("en-GB");
+const localizer = momentLocalizer(moment);
+
+export function HomePage() {
+  const [eventsData, setEventsData] = useState(events);
+
+  //criar uma constante para Styles de calendar
+  const myStyle = {
+      display: "flex",
+      backgroundColor: "white",
+      color: "black",
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center"
+  };
+
+  const handleSelect = ({ start, end }) => {
+    console.log(start);
+    console.log(end);
+    const title = window.prompt("New Event name");
+    if (title)
+      setEventsData([
+        ...eventsData,
+        {
+          start,
+          end,
+          title,
+        },
+      ]);
+  };
+  return (
+    <div className={styles.wrapperCalendar}>
+      <Calendar
+        views={["day", "agenda", "work_week", "month"]}
+        selectable
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={eventsData}
+        style={myStyle}
+        onSelectEvent={(event) => alert(event.title)}
+        onSelectSlot={handleSelect}
+      />
+    </div>
+  );
+}
--- src/page/LandingPage/Section/ThirdSection.jsx
+++ src/page/LandingPage/Section/ThirdSection.jsx
@@ -4,57 +4,96 @@ import { Heading3 } from "../../../components/util/Heading3";
 import TextField from "@mui/material/TextField";
 import { Button } from "../../../components/util/Button";
 import { styled } from "@mui/material/styles";
+import { Navigate } from "react-router-dom";
 
 export default function ThirdSection() {
   const CssTextField = styled(TextField)({
-    "& label.Mui-focused": {
-      color: "white",
-    },
-    "& .MuiInput-underline:after": {
-      borderBottomColor: "white",
-    },
+    "& label.Mui-focused": { color: "white" },
+    "& label": { color: "white" },
+    "& .MuiInput-underline:after": { borderBottomColor: "white" },
     "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "White",
-      },
-      "&:hover fieldset": {
-        borderColor: "white",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "white",
-      },
-      "& label.Mui-focused": {
-        color: "white",
-    },
+      "& fieldset": { borderColor: "White" },
+      "&:hover fieldset": { borderColor: "white" },
+      "&.Mui-focused fieldset": { borderColor: "white" },
+      "& input": { color: "white" },
+      "& textarea": { color: "white" },
     },
   });
+
+  // Logic for sending WhatsApp message
+  const sendForm = (event) => {
+    event.preventDefault();
+    try {
+      // const nameInput = document.getElementById("nameInput").value;
+      const numberInput = document.getElementById("numberInput").value;
+      const message = document.getElementById("messageInput").value;
+
+      const whatsappLink = `https://api.whatsapp.com/send?phone=${numberInput}&text=${encodeURIComponent(
+        message
+      )}`;
+      Navigate(whatsappLink);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <TitleTopic title="Contato" id="contacts" />
-      <form className={styles.formContatos}>
-        <Heading3 color="white" fontSize="large" text="Informações pessoais:" />
-        <CssTextField
-          id="outlined-basic"
-          label="Informe seu Nome"
-          variant="outlined"
-          fullWidth
-          
-        />
-        <CssTextField
-          id="outlined-basic"
-          label="Informe seu Número"
-          variant="outlined"
-          fullWidth
-        />
-        <Heading3 color="white" fontSize="large" text="Assunto de Interesse" />
-        <CssTextField
-          id="outlined-basic"
-          label="Informe sua mensagem"
-          variant="outlined"
-          fullWidth
-        />
-        <Button color="red" buttonName="ENVIAR" />
-      </form>
+      <div className={styles.wrapperContacts}>
+        <form className={styles.formContatos}>
+          <Heading3
+            color="white"
+            fontSize="large"
+            text="Informações pessoais:"
+          />
+          <CssTextField
+            id="nameInput"
+            label="Informe seu Nome"
+            variant="outlined"
+            fullWidth
+          />
+          <CssTextField
+            id="numberInput"
+            label="Informe seu Número"
+            variant="outlined"
+            inputMode="numeric"
+            fullWidth
+          />
+          <Heading3
+            color="white"
+            fontSize="large"
+            text="Assunto de Interesse"
+          />
+          <CssTextField
+            id="messageInput"
+            label="Informe sua mensagem"
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={4}
+          />
+          <div className={styles.btn}>
+            <Button color="red" buttonName="ENVIAR" onClick={sendForm} />
+          </div>
+        </form>
+        <div className={styles.infoContatos}>
+          <Heading3 color="white" fontSize="large" text="Endereço:" />
+          <Heading3
+            color="white"
+            fontSize="small"
+            text="Rua 13 Norte, Lote 04 – Ed. Ilha de Manhattan – Águas Claras, Brasília – DF."
+          />
+          <Heading3 color="white" fontSize="large" text="Horário:" />
+          <Heading3
+            color="white"
+            fontSize="small"
+            text="Segunda a sexta: 09h às 20h - Sábado: 9h às 19h"
+          />
+          <Heading3 color="white" fontSize="large" text="Telefone:" />
+          <Heading3 color="white" fontSize="small" text="+55 (94) 98118-3574" />
+        </div>
+      </div>
     </>
   );
 }
