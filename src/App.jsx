daa8529697eb21b7fd937bb647e563a63e20fa5d
Mr-Lucasz <l.cunha14.lc@gmail.com>
2023-10-19 21:28:05 -0300

    Feature Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { LandingPage } from "./page/LandingPage/LandingPage.jsx";
 import { HomePage } from "./page/Home/HomePage.jsx";
 import "./App.module.css";
 import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "./page/Login/LoginPage.jsx";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage/>} />
     </Routes>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,51 @@
-import styles from "./Header.module.css";
-import barberOnLogo from "../assets/Logotipo.svg";
-import { Button } from "./util/Button.jsx";
-import { Navbar } from "./util/Navbar.jsx";
+import styles from './Header.module.css';
+import barberOnLogo from '../assets/Logotipo.svg';
+import { Button } from './util/Button.jsx';
+import { Navbar } from './util/Navbar.jsx';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export function Header() {
+export function Header({ showButton = true, showNavbar = true, centeredLogo = false }) {
+  const isCentered = !showButton && !showNavbar;
+  const headerClassNames = isCentered ? `${styles.header} ${styles.centered}` : styles.header;
+
+  // Adicione uma classe condicional para centralizar o logotipo
+  const logoClassNames = centeredLogo ? styles.centeredLogo : '';
+
   const links = [
-    { href: "#home", text: "Home" },
-    { href: "#services", text: "Serviços" },
-    { href: "#contacts", text: "Contatos" },
+    { href: '#home', text: 'Home' },
+    { href: '#services', text: 'Serviços' },
+    { href: '#contacts', text: 'Contatos' },
   ];
 
   return (
-    <header className={styles.header}>
+    <header className={headerClassNames}>
+      {/* Adicione a classe condicional ao logotipo */}
       <img
-        className="logotipo-barbearia"
+        className={`logotipo-barbearia ${logoClassNames}`}
         src={barberOnLogo}
         alt="logotipo da barbearia"
       />
-      <Navbar links={links} />
-      <Button color="white" size="large" buttonName="ENTRAR">
-        ENTRAR
-      </Button>
+      {showNavbar && <Navbar links={links} />}
+      {showButton && (
+        <Link to="/login">
+          <Button color="white" size="large" buttonName="ENTRAR">
+            ENTRAR
+          </Button>
+        </Link>
+      )}
     </header>
   );
 }
+
+Header.propTypes = {
+  showButton: PropTypes.bool,
+  showNavbar: PropTypes.bool,
+  centeredLogo: PropTypes.bool, // Adicione a propriedade centeredLogo
+};
+
+Header.defaultProps = {
+  showButton: true,
+  showNavbar: true,
+  centeredLogo: false, // Defina o valor padrão para centeredLogo
+};
