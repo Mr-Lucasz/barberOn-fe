8aab6b2312e25999d7dc25da0cf5157fe31ab910
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2023-10-27 16:25:40 -0300

    finalização Componente Accordion

--- src/components/util/Accordion.jsx
+++ src/components/util/Accordion.jsx
@@ -1,10 +1,24 @@
+//accordion com props de header e body
 
+import{ useState } from "react";
+import styles from "./Accordion.module.css";
+import { PropTypes } from 'prop-types';
+export const Accordion = ({ header, body }) => {
 
-
-export default function Accordion{
-
+    const [open, setOpen] = useState(false);
     
-
-
-    
+    return (
+        <div className={styles.wrapperAccordion}>
+        <div className={styles.headerAccordion} onClick={() => setOpen(!open)}>
+            <span>{header}</span>
+            <span>{open ? "-" : "+"}</span>
+        </div>
+        {open && <div className={styles.bodyAccordion}>{body}</div>}
+        </div>
+    );
 }
+
+Accordion.propTypes = {
+    header: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+};
