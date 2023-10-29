7523fe4560782414b02631cf327d817749f77ee5
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2023-10-29 02:22:20 -0300

    Atualização header e Icone de Entrar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,53 @@
+import PropTypes from "prop-types";
 import styles from "./Header.module.css";
 import barberOnLogo from "../assets/Logotipo.svg";
 import { Button } from "./util/Button.jsx";
 import { Navbar } from "./util/Navbar.jsx";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
+import enterIcon from "../assets/Enter.svg";
 
 export function Header({
   showButton = true,
   showNavbar = true,
-  centeredLogo = false,
+  showIconEnter = true,
 }) {
-  const isCentered = !showButton && !showNavbar;
-  const headerClassNames = isCentered
-    ? `${styles.header}`
-    : `${styles.header} ${centeredLogo ? styles.centeredLogo : ""}`;
-
   const links = [
     { href: "#home", text: "Home" },
     { href: "#services", text: "Serviços" },
     { href: "#contacts", text: "Contatos" },
   ];
+  const isLoginPage = location.pathname === "/login";
 
   return (
-    <header className={headerClassNames}>
+    <header
+      className={`${styles.header} ${isLoginPage ? styles.loginHeader : ""}`}
+    >
       <Link to="/">
         <img
-          className="logotipo-barbearia"
+          className={styles.logo}
           src={barberOnLogo}
           alt="logotipo da barbearia"
         />
       </Link>
       {showNavbar && <Navbar links={links} />}
-      {showButton && (
+      <div className={styles.button}>
+        {showButton && (
+          <Link to="/login">
+            <Button color="white" size="large" buttonName="ENTRAR">
+              ENTRAR
+            </Button>
+          </Link>
+        )}
+      </div>
+      {showIconEnter && (
         <Link to="/login">
-          <Button color="white" size="large" buttonName="ENTRAR">
-            ENTRAR
-          </Button>
+          <div className={styles.btnEntrar}>
+            <img
+              className={styles.iconEnter}
+              src={enterIcon}
+              alt="icone de entrar"
+            />
+          </div>
         </Link>
       )}
     </header>
@@ -45,11 +57,11 @@ export function Header({
 Header.propTypes = {
   showButton: PropTypes.bool,
   showNavbar: PropTypes.bool,
-  centeredLogo: PropTypes.bool,
+  showIconEnter: PropTypes.bool,
 };
 
 Header.defaultProps = {
   showButton: true,
   showNavbar: true,
-  centeredLogo: false,
+  showIconEnter: true,
 };
