2d08585113ab67c03ab6a6fc4dc5ceed18551a98
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2023-10-30 21:24:21 -0300

    :feature: Login

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,31 @@
 import styles from "./Form.module.css";
 import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
 import { Button } from "./util/Button.jsx";
 
 export function Form() {
+  const handleClick = function (event) {
+    event.preventDefault();
+    try {
+      const email = document.getElementById("outlined-email").value;
+      const senha = document.getElementById("outlined-password").value;
+      console.log(email);
+      console.log(senha);
+      //regex validação email
+      const regexEmail = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
+      if (email === "" || senha === "" || senha === undefined || email === undefined) {
+        alert("Preencha todos os campos");
+        return;
+      } else if (!regexEmail.test(email)) {
+        alert("Email inválido");
+        return;
+      }
+      alert("Login realizado com sucesso");
+    } catch (error) {
+      console.log(error);
 
+      alert("Erro ao realizar login");
+    }
+  };
 
   return (
     <form className={styles.form}>
@@ -12,12 +33,17 @@ export function Form() {
         <h1 className={styles.formTitle}>Login</h1>
         <span className={styles.formSubtitle}>
           Please enter your credentials to proceed
+          <p>
+            Voce pode se cadastrar{" "}
+            <a className={styles.here} href="/">
+              clicando aqui !
+            </a>
+          </p>
         </span>
-        <p>Login Here</p>
       </header>
       <section className={styles.formSection}>
         <TextField
-          id="outlined-basic"
+          id="outlined-email"
           label="Informe seu Email"
           variant="outlined"
           required={true}
@@ -25,21 +51,17 @@ export function Form() {
           fullWidth
         />
         <TextField
-          id="outlined-basic"
+          id="outlined-password"
           label="Informe sua Senha"
           variant="outlined"
           required={true}
           type="password"
-          endAdornment={
-            <InputAdornment position="end"></InputAdornment>
-            //icone de toggle password - fa fa-eye ou fa fa-eye-slash - inicia code abaixo:
-          }
         />
 
         <span>Forgot Password ?</span>
       </section>
       <footer className={styles.formFooter}>
-        <Button color="blue" size="large" buttonName="ENTRAR"/>
+        <Button color="blue" size="large" buttonName="ENTRAR" onClick={handleClick} />
       </footer>
     </form>
   );
--- src/page/LandingPage/Section/FIrstSection.jsx
+++ src/page/LandingPage/Section/FIrstSection.jsx
@@ -1,43 +1,39 @@
 import styles from "./FirstSection.module.css";
-import bannerBarberOn from "../../../assets/Banner.svg";
-import ScrollCarousel from "scroll-carousel-react";
+import bannerBarberOn from "../../../assets/Banner.svg"
 import imgSection1 from "../../../assets/imgSection1.svg";
-import imgSection2 from "../../../assets/imgSection2.svg";
-import imgSection3 from "../../../assets/imgSection3.svg";
 
 export function FirstSection() {
-  // Array de caminhos das imagens
-  const imagePaths = [
-    imgSection1,
-    imgSection2,
-    imgSection3,
-    
-  ];
-
   return (
-    <section id="home">
+    <section id="home" className={styles.home}>
       <img
         className={styles.bannerBarberOn}
         src={bannerBarberOn}
         alt="banner da barbearia"
       />
-      <ScrollCarousel
-        autoplay
-        reinit
-        autoplaySpeed={1}
-        infinite={true}
-        speed={1}
-      >
-        {imagePaths.map((imagem) => (
-          <div key={imagem} className={styles.carouselItem}>
-            <img
-              src={imagem}
-              alt={`Imagem ${imagem + 1}`}
-              className={styles.carouselImage}
-            />
-          </div>
-        ))}
-      </ScrollCarousel>
+
+      <div className={styles.imageContainer}>
+        <img
+          src={imgSection1}
+          alt="Imagem 1"
+          className={styles.carouselImage}
+        />
+        {/* <img
+          src={imgSection1}
+          alt="Imagem 2"
+          className={styles.carouselImage}
+        />
+        <img
+          src={imgSection1}
+          alt="Imagem 3"
+          className={styles.carouselImage}
+        />
+        <img
+          src={imgSection1}
+          alt="Imagem 3"
+          className={styles.carouselImage}
+        /> */}
+      </div>
+
     </section>
   );
 }
